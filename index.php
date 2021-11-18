4af77356b36b5b0b4afaaa7bc8bdc660b0967cfd
AnaMorel <84594497+AnaMorel@users.noreply.github.com>
2021-11-17 22:12:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         <label for="check" class="checkbtn">
           <i class="fas fa-bars"></i>
         </label>
-        <label class="logo">Diseño</label>
+        <label class="logo">UCA Sede Gré</label>
 
         <ul>
           <li><a class="active" href="#">Inicio</a></li>
@@ -25,7 +25,25 @@
           <li><a href="#">Otros</a></li>
         </ul>
       </nav>
-      <section></section>
+      <section class="textos-header">
+     <h1> Universidad Cátolica Nuestra Señora de la Asunción </h1>
+     </section>
+     <div class="wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #08f;"></path></svg></div>
+    <main>
+      <section clas="contenedor">
+      <h2 class="titulo">Perspectiva</h2>
+      <div class="contenedor-uca">
+        <img src="images/phpThumb_generated_thumbnailjpg-45.jpg" alt="" class="imagen-about-us"> 
+        <div class="contenido-textos">
+          <h3><span>1</span>Misión</h3>
+          <p>La Universidad Católica “Nuestra Señora de la Asunción”, servicio y contribución de la Iglesia, busca a partir de los valores éticos-cristianos la excelencia en la gestión de la docencia, la investigación y la extensión, contribuyendo a una sociedad justa, solidaria, incluyente, sostenible y respetuosa de la dignidad humana.
+          Según Plan Estratégico 2016-2020, elaborado por el Consejo de la Comunidad Universitaria y aprobado por el Consejo General de Gobierno.</p>
+          <h3><span>2</span>Visión</h3>
+          <p>Institución de referencia en la educación superior en el ámbito nacional y regional, por el aporte substantivo a la investigación, innovación y la responsabilidad social, desde la doctrina social de la Iglesia, por la calidad humana y profesional y por el compromiso cristiano de sus egresados.</p>
+        </div>
+      </section>
+      <select class=""></select>
+   </main>
 
 
 </body>
